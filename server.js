e018dae095dafbef4790ac5f26e6ea30871ff4eb
Brandon971 <r.swinton971@gmail.com>
2023-03-17 16:04:47 +0200

    back2

--- server.js
+++ server.js
@@ -13,11 +13,8 @@ app.use(cors(corsOptions));
 app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true")
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
   next();
 });
 
