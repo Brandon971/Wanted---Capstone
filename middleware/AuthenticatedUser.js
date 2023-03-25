8b385b561a9ff0ddb38765c1986baeca5e771a84
Brandon971 <r.swinton971@gmail.com>
2023-03-25 17:33:49 +0200

    update

--- middleware/AuthenticatedUser.js
+++ middleware/AuthenticatedUser.js
@@ -13,13 +13,11 @@ function createToken(user) {
 
 function verifyAToken(req, res, next) {
     try{
-        const token = req.cookies["Legit"] !== null ? req.cookies["Legit"] :
-        "Register Please";
+        const token = req.headers["Legit"]?.split(' ')[1];
         let isValid = null;
         if(token !== "Register Please") {
             isValid = verify(token, process.env.SECRET_KEY);
             if(isValid) {
-                req.authenticated = true;
                 next()
             }else{
                 res.status(400).json({err:"Register Please"})
